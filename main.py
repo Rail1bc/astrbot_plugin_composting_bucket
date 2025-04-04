8f219bcecb3fbfcca72abf474ff7828a429395b2
Raila23 <3271405327@qq.com>
2025-04-04 20:13:44 +0800

    测试逻辑和输出，暂时不进行真正地处理

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+from distutils.command.check import check
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.core.log import LogManager
+from astrbot.api.provider import ProviderRequest
+
+from typing import List, Dict
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context , config:dict ):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        self.config = config
+        self.logger = LogManager.GetLogger(log_name="CompostingBucket")
 
+    # --- 事件钩子 ---
+    @filter.on_llm_request()
+    async def query_memory(self, event: AstrMessageEvent, req: ProviderRequest):
+        """[事件钩子] 在 LLM 请求前，检查历史上下文长度并处理"""
+        await self.check_message_length(self)
+
+    # 检查消息长度
+    async def check_message_length(self, req: ProviderRequest):
+        self.logger.info("检查消息上下文长度,当前长度:" + str(len(req.contexts)))
+        if(len(req.contexts) >= self.config.max_len*2):
+            self.logger.info("消息上下文长度到达标准:" + self.config.max_len + ",开始弹出旧消息")
+            await self.pop_messages(self)
+
+    # 弹出上下文历史中的旧消息
+    async def pop_messages(self ,req: ProviderRequest):
+        self.logger.info("开始弹出旧消息")
+        self.logger.info(str(req.contexts))
+
+
+
+    # --- 插件生命周期方法 ---
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
